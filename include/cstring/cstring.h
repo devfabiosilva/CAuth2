8b09797e777b397a3b1a1be9ba8708a0e4265e35
Fabio Pereira da Silva <fabioegel@gmail.com>
2022-08-14 00:28:11 -0300

    Adding cstring array structure

--- include/cstring/cstring.h
+++ include/cstring/cstring.h
@@ -14,22 +14,37 @@ enum ctype_e {
 
 #define _CSTRING_ALIGN_SIZE (size_t)16
 
-typedef struct cstring_t {
-    // BEGIN COMMON HEADER
-    uint64_t magic;
-    int32_t ctype;
-    uint8_t pad1[4];
-    const char *header_description;
+#define C_OBJECT_HEADER \
+    uint64_t magic; \
+    int32_t ctype; \
+    uint8_t pad1[4]; \
+    const char *header_description; \
     uint64_t size;
-    // END COMMON HEADER
+
+typedef struct cstring_t {
+    C_OBJECT_HEADER
+    struct cstring_t *previous;
+    struct cstring_t *next;
     uint64_t string_size;
-    //uint8_t pad2[8];
     char *string;
 } __attribute__((aligned(_CSTRING_ALIGN_SIZE))) CSTRING;
 
-_Static_assert(sizeof(CSTRING)==(3*_CSTRING_ALIGN_SIZE), "Error align CSTRING");
+_Static_assert(sizeof(CSTRING)==(4*_CSTRING_ALIGN_SIZE), "Error align CSTRING");
 _Static_assert(sizeof(uint64_t)>=sizeof(size_t), "Arch error");
 _Static_assert(sizeof(CSTRING)==offsetof(CSTRING, string)+sizeof(((CSTRING *)NULL)->string), "Error align string");
+
+typedef struct cstring_array_t {
+    C_OBJECT_HEADER
+    uint32_t number_of_elements;
+    uint8_t pad2[4+8];
+    uint64_t total_string_size;
+    uint64_t total_cstring_objects_size;
+    uint64_t total_size;
+    CSTRING *cstring_objects;
+} __attribute__((aligned(_CSTRING_ALIGN_SIZE))) CSTRING_ARRAY;
+
+_Static_assert(sizeof(CSTRING_ARRAY)==(5*_CSTRING_ALIGN_SIZE), "Error align CSTRING_ARRAY");
+_Static_assert(sizeof(CSTRING_ARRAY)==offsetof(CSTRING_ARRAY, cstring_objects)+sizeof(((CSTRING_ARRAY *)NULL)->cstring_objects), "Error align string array");
 
 #define CSTR_ALIGN(d, s) \
     d=s+1;\
@@ -40,6 +55,9 @@ _Static_assert(sizeof(CSTRING)==offsetof(CSTRING, string)+sizeof(((CSTRING *)NUL
 
 #define _CSTR_PTR_SELF_CONTAINED(ptr) \
     (char *)(((char *)ptr)+offsetof(CSTRING, string)+sizeof(((CSTRING *)0)->string))
+
+#define _CSTRING_ARRAY_PTR_SELF_CONTAINED(ptr) \
+    (char *)(((char *)ptr)+offsetof(CSTRING_ARRAY, cstring_objects)+sizeof(((CSTRING_ARRAY *)0)->cstring_objects))
 
 #define CSTR_COPY_SELF_CONTAINED(size, size_aligned) \
     cstr->string_size=(uint64_t)size; \
